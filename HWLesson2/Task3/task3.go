906c36ae624a8a262397deff1ed3fc5b1223ae41
SvyatSanders <sv876744@gmail.com>
2019-12-16 18:29:24 +0300

    Added changes for 3rd task (problem solved with math/big)

--- HWLesson2/Task3/task3.go
+++ HWLesson2/Task3/task3.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+)
 
 // 3. Написать функцию, которая последовательно выводит на экран 100 первых чисел Фибоначчи, начиная от 0.
 // Числа Фибоначчи определяются соотношениями Fn=Fn-1 + Fn-2.
@@ -28,10 +31,33 @@ func fibi(n int) int {
 
 func main() {
 	for i := 1; i <= 100; i++ {
-		fmt.Println(fibi(i))
+		//fmt.Println(fibi(i))
 		//fmt.Println(Fib(i))
 	}
+	fibMB()
 }
 
-// a, b = b, a+b - непонятен пример
-// просьба объяснить
+func fibMB() {
+	// Initialize two big ints with the first two numbers in the sequence.
+	a := big.NewInt(0)
+	b := big.NewInt(1)
+
+	// Initialize limit as 10^99, the smallest integer with 100 digits.
+	var limit big.Int
+	limit.Exp(big.NewInt(10), big.NewInt(99), nil)
+
+	// Loop while a is smaller than 1e100.
+	for a.Cmp(&limit) < 0 {
+		// Compute the next Fibonacci number, storing it in a.
+		a.Add(a, b)
+		// Swap a and b so that b is the next number in the sequence.
+		a, b = b, a
+	}
+	fmt.Println(a) // 100-digit Fibonacci number
+
+	// Test a for primality.
+	// (ProbablyPrimes' argument sets the number of Miller-Rabin
+	// rounds to be performed. 20 is a good value.)
+	fmt.Println(a.ProbablyPrime(20))
+
+}
